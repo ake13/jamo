1a345d0c2d4c38bae1f11d5eb6b3f8a2ba17a313
nan0shell <nanoshell@proton.me>
2023-11-24 10:08:31 +0000

    feature: use mutagen library instead of id3v2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import asyncio, requests, glob, sys, os, time, argparse, subprocess
+from mutagen.easyid3 import EasyID3
 from shazamio import Shazam
 from alive_progress import alive_bar
 
@@ -61,16 +62,14 @@ def write_log(dst_dir,log_filename,track_filename):
   with open(dst_dir+"/"+log_filename+".log", "a") as log:
       log.write("["+d+"]  --- "+track_filename+'\n')
 
-# Autmatic tag the rename files.. but i'm to lazy now to write a class to deal with tags, so I used id3v2 :) apt-get install id3v2
-def add_tag(file_name,artist,song_name,genres):
-  try:
-      from subprocess import DEVNULL  # Python 3.
-  except ImportError:
-      DEVNULL = open(os.devnull, 'wb')
-  p1 = subprocess.Popen(["/usr/bin/id3v2", "-D", file_name], stdout=DEVNULL, stderr=DEVNULL)
-  p1.wait()
-  p2 = subprocess.Popen(["/usr/bin/id3v2", "-a", artist, "-t", song_name, "-g", genres, file_name ])
-  p2.wait()
+# Automatically clean and tag the renamed files
+def add_tag(artist,song_name,primary_genre,file_name):
+  track_tags = EasyID3(file_name)
+  track_tags.delete()
+  track_tags["title"] = song_name
+  track_tags["artist"] = artist
+  track_tags["genre"] = primary_genre
+  track_tags.save()
 
 # Small function that parses the video url from shazam to retrive the youtube video. Just playing with things while time fly's by.
 def get_youtube_url(shazam_video_url):
